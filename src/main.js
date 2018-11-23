89483b524fbf3b9e34cb0a3b5d44927983d70dac
coder_xue <857986029@qq.com>
2018-11-23 11:34:10 +0800

    change

--- src/main.js
+++ src/main.js
@@ -7,12 +7,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastClick'  //安装:cnpm install fastclick --save
+import VueAwesomeSwiper from 'vue-awesome-swiper'  //轮播图插件
+import 'swiper/dist/css/swiper.css' //轮播图插件样式文件
 import 'styles/reset.css'  
 import 'styles/border.css'  //解决移动端1px边框问题
 import 'styles/iconfont.css' //字体图标
 
 Vue.config.productionTip = false
 fastClick.attach(document.body); //解决移动端点击事件300毫秒延迟问题
+Vue.use(VueAwesomeSwiper, /* { default global options } */) 
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,52 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+		    <!-- slides -->
+		    <swiper-slide v-for="item of swiperList" :key="item.id">
+		    	<img class="swiper-img" :src="item.imgUrl">
+		    </swiper-slide>
+		    
+		    <!-- Optional controls -->
+		    <div class="swiper-pagination"  slot="pagination"></div>
+		    
+	  	</swiper>
+  	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data() {
+			return {
+				swiperOption: {
+					pagination: '.swiper-pagination', // 分页器
+					loop: true   //循环轮播
+				},
+				swiperList: [{
+					id: '0001',
+					imgUrl: 'https://img1.qunarzz.com/vc/a5/e0/64/c6bd8680fb67247db15df6057b.jpg'
+				},{
+					id: '0002',
+					imgUrl: 'https://img1.qunarzz.com/vc/a4/36/26/07c5904b545d66b3abe72e9dfa.jpg'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped="">
+	/*scoped 表示只修饰当前组件的class，不会对其他的组件造成影响*/
+
+	/*样式穿透，表示只要wrapper这个类名下出现了该类名，就让该类名的颜色变成白色，这样就不会受scoped的影响了*/
+
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: #fff;
+	.wrapper
+		width: 100%;
+		height: 0;
+		overflow: hidden;
+		padding-bottom: 31.25%;  /*设置高度为宽度的31.25% */
+		background: #eee;
+		.swiper-img
+			width: 100%;
+</style>
