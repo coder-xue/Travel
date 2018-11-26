bef2912179128dc072c7e354a43f67e7d6f1a2ea
coder_xue <857986029@qq.com>
2018-11-26 20:19:46 +0800

    在router/index.js中使用异步加载组件的方式

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,9 @@
 
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/pages/home/Home.vue'
-import City from '@/pages/city/City.vue'
-import Detail from '@/pages/detail/Detail.vue'
+// import Home from '@/pages/home/Home.vue'
+// import City from '@/pages/city/City.vue'
+// import Detail from '@/pages/detail/Detail.vue'
 
 
 Vue.use(Router)
@@ -14,17 +14,20 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      // component: Home
+      component: () => import('@/pages/home/Home.vue') //异步加载组件,表示当需要哪些文件的时候才会去加载,提高了页面首次加载的时间,当app.js大于1MB的时候才建议去使用异步加载组件
     },
     {
       path: '/city',
       name: 'City',
-      component: City
+      // component: City
+      component: () => import('@/pages/city/City.vue')
     },
     {
       path: '/detail/:id',  // 加一个冒号表示动态路由,表示路径前面必须是/detail/,后面可以带参数
       name: 'Detail',
-      component: Detail
+      // component: Detail
+      component: () => import('@/pages/detail/Detail.vue')
     }
   ],
   //每次的路由切换滚动条都会回到最顶部
